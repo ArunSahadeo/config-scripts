41e690b7338f9a1f9a3aa7c94781565ef10da7d1
Arun Sahadeo <arunjamessahadeo@gmail.com>
2017-05-09 22:55:31 +0100

    Update readme script

--- update-readme.sh
+++ update-readme.sh
@@ -4,14 +4,14 @@ readme_count=$()
 
 the_readme=`find . -iname 'readme.md' | sed 's|./||'`
 
-if [ -f $the_readme ]; then
-    readme_count=`git log --oneline $the_readme | wc -l`
+if [ -f "$the_readme" ]; then
+    readme_count=`git log --oneline "$the_readme" | wc -l`
 else
     echo "There is not a README in this directory"
     return
 fi
 
-if [ $readme_count -gt 1 ]; then
+if [ "$readme_count" -gt 1 ]; then
     echo "Your README is up to date"
     exit
 fi
@@ -20,10 +20,14 @@ echo "You need to update your README"
 
 sleep 2
 
-character_count=`wc -m < $the_readme`
+readonly character_count=`wc -m < "$the_readme"`
+readme_characters=`wc -m < "$the_readme"`
 
-if [ -f $the_readme ]; then
-    until [ $character_count -ne "" ]; do
-    nano $the_readme
+if [ -f "$the_readme" ]; then
+    until [ "$readme_characters" -ne "$character_count" ]; do
+    	nano "$the_readme"
+	if [ "$readme_characters" -gt "$character_count" || "$readme_characters" -lt "$character_count" ]; then
+		exit
+	fi
     done
 fi
