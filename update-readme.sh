ca518d708368a49a3162892484aa6854c708d9f4
Arun Sahadeo <arun@wirebox.co.uk>
2017-05-09 16:47:37 +0100

    Update readme script

---
+++ update-readme.sh
@@ -0,0 +1,30 @@
+#!/usr/bin/env bash
+
+readme_count=$()
+
+if [ -f readme.md ]; then
+    readme_count=`git log --oneline readme.md | wc -l`
+elif [ -f README.MD]; then
+    readme_count=`git log --oneline README.MD | wc -l`
+elif [ -f README.md]; then
+    readme_count=`git log --oneline README.md | wc -l`
+else
+    echo "There is not a README in this directory"
+    return
+fi
+
+if [ $readme_count -gt 1 ]; then
+    exit
+fi
+
+echo "You need to update your README"
+
+sleep 2
+
+if [ -f readme.md ]; then
+    nano readme.md
+elif [ -f README.MD]; then
+    nano README.MD
+elif [ -f README.md]; then
+    nano README.md
+fi
